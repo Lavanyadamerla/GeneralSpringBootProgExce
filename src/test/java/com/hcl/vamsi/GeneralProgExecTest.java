73d13b559113ab9ab9b2e67e6f30d0c7bac98669
Lavanyadamerla <darmla.lavanya@gmail.com>
2025-08-11 02:00:11 +0530

    Create GeneralProgExecTest.java

---
+++ src/test/java/com/hcl/vamsi/GeneralProgExecTest.java
@@ -0,0 +1,12 @@
+package com.hcl.vamsi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneralProgExecTest {
+  @Test
+  void contextLoads() {
+    System.out.println("This test passes successfully");
+  }
+}
